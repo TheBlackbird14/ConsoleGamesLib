19fff429e2c0f2598987d03038469bc20d66778f
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2023-04-14 00:53:18 +0200

    tsarted work on place() to place ships

--- games/battleships.c
+++ games/battleships.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 
 #include "../libs/strcap.h"
 #include "../libs/cs50.h"
@@ -8,11 +12,19 @@
 #define missed "\033[37;44m"
 
 void initialize(void);
+void place(void);
+void instructions(void);
+
 void clear(void);
 void waitfor(char input);
-void print(char you[10][10], char enemy[10][10]);
+void quit(void);
 
-char boardp1[10][10] = {
+void print(char you[10][10], char enemy[10][10]);
+int checkInput(string input);
+int checkPlacement(string coords, char dir);
+
+char boardp1[10][10]; 
+/* = {
 {'X', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 {'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -24,8 +36,9 @@ char boardp1[10][10] = {
 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
 };
-
-char boardp2[10][10] = {
+ */
+char boardp2[10][10];
+/*  = {
 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 {' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' '},
 {' ', ' ', ' ', ' ', ' ', 'O', 'X', 'O', ' ', ' '},
@@ -36,30 +49,110 @@ char boardp2[10][10] = {
 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+}; */
+
+
+char tablenames[2][10] = {{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}, {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}};
+int done = 0;
+int turnp1;
+int turnp2;
+int turns = 0;
+
+string input1;
+string input2;
+
+
+#define MAX_SHIPS 5
+struct shipp1
+{
+    int size;
+    int row;
+    int col;
+    char dir;
 };
 
+struct shipp1 shipsp1[MAX_SHIPS];
 
-char tablenames[2][10] = {{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}, {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}};
+
+struct shipp2
+{
+    int size;
+    int row;
+    int col;
+    char dir;
+};
+
+struct shipp2 shipsp2[MAX_SHIPS];
+
 
 int main(void)
 {
     clear();
-    //initialize();
+    initialize();
+    instructions();
+
+    while (!done) 
+    {
+        if (turns == 0)
+        {
+            place();
+            printf("Press ENTER to start as player one");
+        }
+        else
+        {
+            clear();
+            printf("Press ENTER to continue as player one");
+        }
+        
+        waitfor('\n');
+        turnp1 = 1;
+
+        while (turnp1)
+        {
+            turns++;
+            clear();
+            print(boardp1, boardp2);
+            do
+            {
+                input1 = strup(get_string("Player 1> "));
+            } 
+            while (checkInput(input1) != 0);
 
 
-    printf("//instructions\n");
-    printf("Press ENTER to start as player one");
-    waitfor('\n');
-    clear();
-    print(boardp1, boardp2);
-    printf("ENTER to continue");
-    waitfor('\n');
-    clear();
-    printf("Press ENTER to continue as player two");
-    waitfor('\n');
-    clear();
-    print(boardp2, boardp1);
+            //check if was hit
+            printf("%s\n", input1);
 
+            //if miss
+            turnp1 = 0;
+            turnp2 = 1;
+            printf("ENTER to end turn");
+            waitfor('\n');
+        }
+
+        clear();
+        printf("ENTER to continue as Player 2");
+        waitfor('\n');
+
+        while (turnp2)
+        {
+            turns++;
+            clear();
+            print(boardp2, boardp1);
+            do
+            {
+                input2 = strup(get_string("Player 2> "));
+            } 
+            while (checkInput(input2) != 0);
+
+
+            printf("%s\n", input2);
+
+            turnp2 = 0;
+            printf("ENTER to end turn");
+            waitfor('\n');
+        }
+
+    }
 }
 
 void clear(void)
@@ -83,12 +176,12 @@ void print(char you[10][10], char enemy[10][10])
 {
 
     //print own sea
-    printf("Your Sea: \n ");
+    printf("Your Sea: \n\n ");
     for (int i = 0; i < 10; i++)
     {
-        printf("%c ", tablenames[0][i]);
+        printf(" | %c", tablenames[0][i]);
     }
-    printf("\n");
+    printf("\n\n");
     
     for (int i = 0; i < 10; i++)
     {
@@ -98,22 +191,22 @@ void print(char you[10][10], char enemy[10][10])
             //normsal sea (nothing)
             if (you[i][j] == ' ')
             {
-                printf(" %s %s", bgBlue, off);
+                printf(" | %s %s", bgBlue, off);
             }
             //ship placed on tile
             if (you[i][j] == 'X')
             {
-                printf(" %s %s", bgWhite, off);
+                printf(" | %s %s", bgWhite, off);
             }
             //shot fires but missed
             if (you[i][j] == 'O')
             {
-                printf(" %sO%s", missed, off);
+                printf(" | %sO%s", missed, off);
             }
             //shot fired and hit ship
             if (you[i][j] == 'H')
             {
-                printf(" %sX%s", hitship, off);
+                printf(" | %sX%s", hitship, off);
             }
         }
         printf("\n\n"); 
@@ -122,35 +215,31 @@ void print(char you[10][10], char enemy[10][10])
 
 
     //print enemy sea
-    printf("Enemy Sea: \n ");
+    printf("Enemy Sea: \n\n ");
 
     for (int i = 0; i < 10; i++)
     {
-        printf("%c ", tablenames[0][i]);
+        printf(" | %c", tablenames[0][i]);
     }
-    printf("\n");
+    printf("\n\n");
 
     for (int i = 0; i < 10; i++)
     {
         printf("%c", tablenames[1][i]);
         for (int j = 0; j < 10; j++)
         {
-            //empty sea
-            if (enemy[i][j] == ' ')
+            if (enemy[i][j] == 'O') //shot fired and missed
             {
-                printf(" %s %s", bgBlue, off);
+                printf(" | %sO%s", missed, off);
             }
-            //shot fired and missed
-            if (enemy[i][j] == 'O')
+            else if (enemy[i][j] == 'H') //shot fired and ship hit
             {
-                printf(" %sO%s", missed, off);
+                printf(" | %sX%s", hitship, off);
             }
-            //shot fired and ship hit
-            if (enemy[i][j] == 'H')
+            else
             {
-                printf(" %sX%s", hitship, off);
+                printf(" | %s %s", bgBlue, off);
             }
-            
         }
         printf("\n\n");
     }
@@ -160,4 +249,116 @@ void print(char you[10][10], char enemy[10][10])
 void waitfor(char input)
 {
     while (getchar() != input);
+}
+
+void instructions(void)
+{
+    printf("//instructions\n");
+}
+
+int checkInput(string input)
+{
+    int validnum = 0;
+    int validletter = 0;
+    if (strcmp(input, "QUIT") == 0)
+    {
+        quit();
+    }
+    
+    for (int i = 0; i < 10; i++)
+    {
+        if (input[0] == tablenames[0][i])
+        {
+            validletter = 1;
+            break;
+        }
+        
+    }
+    
+    for (int i = 0; i < 10; i++)
+    {
+        if (input[1] == tablenames[1][i])
+        {
+            validnum = 1;
+            break;
+        }
+        
+    }
+
+    if (validletter == 1 && validnum == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+    
+
+}
+
+void quit(void)
+{
+    clear();
+    printf("Until Next Time!\n");
+    exit(2);
+}
+
+void place(void)
+{
+    shipsp1[0].size = 2;
+    shipsp1[1].size = 3;
+    shipsp1[2].size = 3;
+    shipsp1[3].size = 4;
+    shipsp1[4].size = 5;
+    
+    shipsp2[0].size = 2;
+    shipsp2[1].size = 3;
+    shipsp2[2].size = 3;
+    shipsp2[3].size = 4;
+    shipsp2[4].size = 5;
+
+    clear();
+    print(boardp1, boardp2);
+
+    for (int i = 0; i < MAX_SHIPS; i++)
+    {
+        string rowcol;
+        char dir;
+
+        do
+        {
+            rowcol = strup(get_string("Starting Point of Ship %d with length %d> ", i + 1, shipsp1[i].size));
+
+            if (checkInput(rowcol) != 0)
+            {
+                continue;
+            }
+
+            dir = toupper(get_char("Direction of ship (u, d, l, r)> "));
+
+            if (dir != 'u' && dir != 'd' && dir != 'l' && dir != 'r')
+            {
+                continue;
+            }
+        }
+        while (checkPlacement(rowcol, dir) != 0);
+        
+        
+        shipsp1[i].row = (rowcol[0] - 65);
+        shipsp1[i].col = (rowcol[1] - 48);
+
+        printf("Coords: [%d][%d]\n", shipsp1[i].row, shipsp1[i].col);
+
+        shipsp1[i].dir = dir;
+
+        printf("Dir: %c\n", shipsp1[i].dir);
+       
+    }
+
+}
+
+int checkPlacement(string coords, char dir)
+{
+    //TODO
 }
