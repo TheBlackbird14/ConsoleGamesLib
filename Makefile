9b824c26fec934e86f5865abc1d106bfa841bdb9
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2023-04-13 16:17:45 +0200

    started print board function(not working yet)

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
-cgl: main.c libs/strcap.c games/tictactoe.c libs/strcap.c libs/cs50.c
+cgl: main.c games/tictactoe.c libs/strcap.c libs/cs50.c
 	cc -o games/tictactoe games/tictactoe.c libs/strcap.c libs/cs50.c
 	cc -w -o cgl main.c libs/strcap.c libs/cs50.c
+
+bships: games/battleships.c libs/strcap.c libs/cs50.c libs/colors.h
+	cc -o games/battleships games/battleships.c libs/strcap.c libs/cs50.c
--- games/battleships.c
+++ games/battleships.c
@@ -5,15 +5,46 @@
 #include "../libs/colors.h"
 
 void initialize(void);
-void printp1(void);
-void printp2(void);
+void clear(void);
+void waitfor(char input);
+void print(char you[10][10], char enemy[10][10]);
 
 char boardp1[10][10];
 char boardp2[10][10];
+char tablenames[2][10] = {{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}, {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}};
 
 int main(void)
 {
+    clear();
+    initialize();
+    
+    //testing
+    boardp1[4][8] = 'X';
+    boardp1[6][3] = 'O';
+    boardp1[9][2] = 'h';
 
+    boardp2[5][2] = 'X';
+    boardp2[1][7] = 'O';
+    boardp2[0][3] = 'h';
+
+    printf("//instructions\n");
+    printf("Press ENTER to start as player one");
+    waitfor('\n');
+    clear();
+    print(boardp1, boardp2);
+    printf("ENTER to continue");
+    waitfor('\n');
+    clear();
+    printf("Press ENTER to continue as player two");
+    waitfor('\n');
+    clear();
+    print(boardp2, boardp1);
+
+}
+
+void clear(void)
+{
+    printf("\e[1;1H\e[2J");
 }
 
 void initialize(void)
@@ -28,28 +59,35 @@ void initialize(void)
     }
 }
 
-void printp1(void)
+void print(char you[10][10], char enemy[10][10])
 {
-    printf("Your Sea: \n");
+    printf("Your Sea: \n ");
     for (int i = 0; i < 10; i++)
     {
+        printf("%c ", tablenames[0][i]);
+    }
+    printf("\n");
+    
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%c", tablenames[1][i]);
         for (int j = 0; j < 10; j++)
         {
-            if (boardp1[i][j] == ' ')
+            if (you[i][j] == ' ')
             {
-                printf(bgCyan" "off);
+                printf(bgCyan"  "off);
             }
-            if (boardp1[i][j] == 'X')
+            if (you[i][j] == 'X')
             {
-                printf(bgBlack " " off);
+                printf(bgBlack "  " off);
             }
-            if (boardp1[i][j] == 'O')
+            if (you[i][j] == 'O')
             {
-                printf(bgCyan white"O"off);
+                printf(bgCyan white"O "off);
             }
-            if (boardp1[i][j] == 'h')
+            if (you[i][j] == 'h')
             {
-                printf(bgBlack red"X"off);
+                printf(bgBlack red"X "off);
             }
             
         }
@@ -57,24 +95,39 @@ void printp1(void)
     }
     printf("\n\n");
 
-    printf("Enemy Sea: \n");
+    printf("Enemy Sea: \n ");
+
     for (int i = 0; i < 10; i++)
     {
+        printf("%c ", tablenames[0][i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%c", tablenames[1][i]);
         for (int j = 0; j < 10; j++)
         {
-            if (boardp1[i][j] == ' ')
+            if (enemy[i][j] == ' ')
             {
-                printf(bgCyan" "off);
+                printf(bgCyan"  "off);
             }
-            if (boardp1[i][j] == 'O')
+            if (enemy[i][j] == 'O')
             {
-                printf(bgCyan white"O"off);
+                printf(bgCyan white"O "off);
             }
-            if (boardp1[i][j] == 'h')
+            if (enemy[i][j] == 'h')
             {
-                printf(bgBlack red"X"off);
+                printf(bgBlack red"X "off);
             }
             
         }
+        printf("\n");
     }
+    printf("\n\n");
+}
+
+void waitfor(char input)
+{
+    while (getchar() != input);
 }
