fcc6a0e8cf71fd129208e94e64f98e0cd0e25723
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2023-04-13 18:16:39 +0200

    continued board print function; still buggy

--- games/battleships.c
+++ games/battleships.c
@@ -4,28 +4,48 @@
 #include "../libs/cs50.h"
 #include "../libs/colors.h"
 
+#define hitship "\033[31;47m"
+#define missed "\033[37;44m"
+
 void initialize(void);
 void clear(void);
 void waitfor(char input);
 void print(char you[10][10], char enemy[10][10]);
 
-char boardp1[10][10];
-char boardp2[10][10];
-char tablenames[2][10] = {{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}, {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}};
+char boardp1[10][10] = {
+{'X', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{'H', ' ', ' ', ' ', 'O', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', 'O', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+};
+
+char boardp2[10][10] = {
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', 'O', 'X', 'O', ' ', ' '},
+{' ', ' ', ' ', 'O', ' ', ' ', 'X', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', 'H', 'O', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+};
+
+
+char tablenames[2][10] = {{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}, {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}};
 
 int main(void)
 {
     clear();
-    initialize();
-    
-    //testing
-    boardp1[4][8] = 'X';
-    boardp1[6][3] = 'O';
-    boardp1[9][2] = 'h';
+    //initialize();
 
-    boardp2[5][2] = 'X';
-    boardp2[1][7] = 'O';
-    boardp2[0][3] = 'h';
 
     printf("//instructions\n");
     printf("Press ENTER to start as player one");
@@ -61,6 +81,8 @@ void initialize(void)
 
 void print(char you[10][10], char enemy[10][10])
 {
+
+    //print own sea
     printf("Your Sea: \n ");
     for (int i = 0; i < 10; i++)
     {
@@ -73,28 +95,33 @@ void print(char you[10][10], char enemy[10][10])
         printf("%c", tablenames[1][i]);
         for (int j = 0; j < 10; j++)
         {
+            //normsal sea (nothing)
             if (you[i][j] == ' ')
             {
-                printf(bgCyan"  "off);
+                printf(" %s %s", bgBlue, off);
             }
+            //ship placed on tile
             if (you[i][j] == 'X')
             {
-                printf(bgBlack "  " off);
+                printf(" %s %s", bgWhite, off);
             }
+            //shot fires but missed
             if (you[i][j] == 'O')
             {
-                printf(bgCyan white"O "off);
+                printf(" %sO%s", missed, off);
             }
-            if (you[i][j] == 'h')
+            //shot fired and hit ship
+            if (you[i][j] == 'H')
             {
-                printf(bgBlack red"X "off);
+                printf(" %sX%s", hitship, off);
             }
-            
         }
-        printf("\n"); 
+        printf("\n\n"); 
     }
     printf("\n\n");
 
+
+    //print enemy sea
     printf("Enemy Sea: \n ");
 
     for (int i = 0; i < 10; i++)
@@ -108,21 +135,24 @@ void print(char you[10][10], char enemy[10][10])
         printf("%c", tablenames[1][i]);
         for (int j = 0; j < 10; j++)
         {
+            //empty sea
             if (enemy[i][j] == ' ')
             {
-                printf(bgCyan"  "off);
+                printf(" %s %s", bgBlue, off);
             }
+            //shot fired and missed
             if (enemy[i][j] == 'O')
             {
-                printf(bgCyan white"O "off);
+                printf(" %sO%s", missed, off);
             }
-            if (enemy[i][j] == 'h')
+            //shot fired and ship hit
+            if (enemy[i][j] == 'H')
             {
-                printf(bgBlack red"X "off);
+                printf(" %sX%s", hitship, off);
             }
             
         }
-        printf("\n");
+        printf("\n\n");
     }
     printf("\n\n");
 }
